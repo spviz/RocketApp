4b1d385b501d416eda009f224f3d7dd017e4a9f0
Sergei Podgainy <110125649+spviz@users.noreply.github.com>
2023-01-07 16:53:26 +0400

    Initial Commit

---
+++ RocketApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RocketApp
+//
+//  Created by Podgainy Sergei on 07.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
