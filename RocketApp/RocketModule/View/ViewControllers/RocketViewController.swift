c6551cd6e51eed8c26dfbd6630a78eb17ed059a3
Sergei Podgainy <110125649+spviz@users.noreply.github.com>
2023-02-02 16:44:55 +0400

    Add RocketPresenter

--- RocketApp/RocketModule/View/ViewControllers/RocketViewController.swift
+++ RocketApp/RocketModule/View/ViewControllers/RocketViewController.swift
@@ -7,35 +7,25 @@
 
 import UIKit
 
+protocol RocketViewProtocol: AnyObject {
+    func present(sections: [Section])
+}
+
 final class RocketViewController: UIViewController {
-
-    enum Constants: String {
-        case noData = "Нет данных"
-        case firstStage = "ПЕРВАЯ СТУПЕНЬ"
-        case secondStage = "ВТОРАЯ СТУПЕНЬ"
-        case million = "млн"
-        case ton = "ton"
-        case sec = "sec"
-    }
-
-    private let rocket: Rocket
-    private let dataManager: DataManagerProtocol
-    private let dateFormatter = DateFormatter()
 
     private lazy var collectionView = UICollectionView()
     private lazy var dataSource = setupDataSource()
+    private let presenter: RocketPresenterProtocol
     private var sections = [Section]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupData()
         setupCollectionView()
-        applySnapshot()
+        presenter.getData()
     }
 
-    init(rocket: Rocket, dataManager: DataManagerProtocol) {
-        self.rocket = rocket
-        self.dataManager = dataManager
+    init(presenter: RocketPresenterProtocol) {
+        self.presenter = presenter
         super .init(nibName: nil, bundle: nil)
     }
 
@@ -44,78 +34,16 @@ final class RocketViewController: UIViewController {
     }
 
     func reloadCollectionView() {
-        setupData()
-        applySnapshot()
+        presenter.getData()
     }
 }
 
-// MARK: - Configure Data
+// MARK: - RocketViewProtocol
 
-private extension RocketViewController {
-    func setupData() {
-
-        dateFormatter.dateFormat = "d MMMM, yyyy"
-
-        let height = dataManager.getSelectedUnit(for: .height) == .metric
-        ? String(rocket.height.meters)
-        : String(rocket.height.feet)
-
-        let diameter = dataManager.getSelectedUnit(for: .diameter) == .metric
-        ? String(rocket.diameter.meters)
-        : String(rocket.diameter.feet)
-
-        let mass = dataManager.getSelectedUnit(for: .mass) == .metric
-        ? String(rocket.mass.kg)
-        : String(rocket.mass.lb)
-
-        let payloadWeights = dataManager.getSelectedUnit(for: .payloadWeights) == .metric
-        ? String(rocket.payloadWeights[0].kg)
-        : String(rocket.payloadWeights[0].lb)
-
-        let burnTimeFirstStage: String
-        let burnTimeSecondStage: String
-
-        if let burnTime = rocket.firstStage.burnTimeSec {
-            burnTimeFirstStage = String(burnTime) + " " + Constants.sec.rawValue
-        } else {
-            burnTimeFirstStage = Constants.noData.rawValue
-        }
-
-        if let burnTime = rocket.secondStage.burnTimeSec {
-            burnTimeSecondStage = String(burnTime) + " " + Constants.sec.rawValue
-        } else {
-            burnTimeSecondStage = Constants.noData.rawValue
-        }
-
-        let headSection = Section(type: .header, items: [
-            .header(rocket.flickrImages[Int.random(in: 0...rocket.flickrImages.count - 1)], rocket.name)])
-
-        let horizontalSection = Section(type: .horizontal, items: [
-            .info(dataManager.getSelectedUnit(for: .height) == .metric ? .heightMetric : .heightImperial, height),
-            .info(dataManager.getSelectedUnit(for: .diameter) == .metric ? .diameterMetric : .diameterImperial, diameter),
-            .info(dataManager.getSelectedUnit(for: .mass) == .metric ? .massMetric : .massImperial, mass),
-            .info(dataManager.getSelectedUnit(for: .payloadWeights) == .metric ? .payloadWeightsMetric : .payloadWeightsImperial, payloadWeights)])
-
-        let infoSection = Section(type: .vertical, items: [
-            .info(.firstFlight, dateFormatter.string(from: rocket.firstFlight)),
-            .info(.country, rocket.country),
-            .info(.costPerLaunch,
-                  "$" + String(format: "%.0f", Double(rocket.costPerLaunch) / 1000000) + " " + Constants.million.rawValue
-                 )])
-
-        let firstStageSection = Section(title: Constants.firstStage.rawValue, type: .vertical, items: [
-            .info(.engines, String(rocket.firstStage.engines)),
-            .info(.fuelAmountTons, String(format: "%.0f", rocket.firstStage.fuelAmountTons) + " " + Constants.ton.rawValue),
-            .info(.burnTimeSec, burnTimeFirstStage)])
-
-        let secondStageSection = Section(title: Constants.secondStage.rawValue, type: .vertical, items: [
-            .info(.engines, String(rocket.secondStage.engines)),
-            .info(.fuelAmountTons, String(format: "%.0f", rocket.secondStage.fuelAmountTons) + " " + Constants.ton.rawValue),
-            .info(.burnTimeSec, burnTimeSecondStage)])
-
-        let buttonSection = Section(type: .button, items: [.button])
-
-        sections = [headSection, horizontalSection, infoSection, firstStageSection, secondStageSection, buttonSection]
+extension RocketViewController: RocketViewProtocol {
+    func present(sections: [Section]) {
+        self.sections = sections
+        applySnapshot()
     }
 }
 
@@ -277,7 +205,7 @@ private extension RocketViewController {
 
 extension RocketViewController {
     func presentSettings() {
-        let presenter = SettingsPresenter(dataManager: dataManager)
+        let presenter = SettingsPresenter(dataManager: DataManager())
         let settingsViewController = SettingsViewController(presenter: presenter)
         presenter.settingsView = settingsViewController
         settingsViewController.reloadData = { [weak self] in
@@ -287,7 +215,7 @@ extension RocketViewController {
     }
 
     func pushLaunches() {
-        let presenter = LaunchesPresenter(selectedRocketID: rocket.id, selectedRocketName: rocket.name)
+        let presenter = LaunchesPresenter(selectedRocketID: "rocket.id", selectedRocketName: "rocket.name")
         let launchesViewController = LaunchesViewController(presenter: presenter)
         presenter.launchesView = launchesViewController
         self.navigationController?.pushViewController(launchesViewController, animated: true)
